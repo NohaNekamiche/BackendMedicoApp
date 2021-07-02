36038e0002a78ca057dc9951f5c9caaf666fc635
Selmane Lamia <hl_selmane@esi.dz>
2021-07-02 20:07:20 +0100

    merge to master

--- src/Routers.ts
+++ src/Routers.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from "express";
 import auth from "../src/Authentification/Routes/auth";
-import traitement from "../src/Traitements/Routes/TraitementRoute";
+import traitement from "./Traitement/Routes/TraitementRoute";
 
 
 const routes = Router();
