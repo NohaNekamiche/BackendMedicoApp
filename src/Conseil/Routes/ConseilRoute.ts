7263f435530d7df65606a6c2efd0d6e5b0a4c911
Selmane Lamia <hl_selmane@esi.dz>
2021-07-04 23:30:18 +0100

    fix getconseilbyidpatient

--- src/Conseil/Routes/ConseilRoute.ts
+++ src/Conseil/Routes/ConseilRoute.ts
@@ -6,4 +6,6 @@ import ConseilController from "../Controllers/ConseilController";
 const router=Router();
 router.get('/getAllConseil',ConseilController.getAllConseil);
 router.post('/addConseil',ConseilController.addConseil);
+router.get('/getConseilsByIdPatient/:idPatient',ConseilController.getConseilsByIdPatient);
+
 export default router;
